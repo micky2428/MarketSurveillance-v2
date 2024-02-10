83c6d016cd2e3caf5737cfdf78d3a600dd38b4d9
micky2428 <micky2428@gmail.com>
2024-02-11 02:41:07 +0800

    1130211

--- app/src/main/java/com/example/marketsurveillance/PhotoUpload.kt
+++ app/src/main/java/com/example/marketsurveillance/PhotoUpload.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -37,7 +38,7 @@ class GoogleDriveLauncher(private val activity: Activity) {
         return account != null
     }
 
-//原本
+    //原本
 //    private fun checkPermission(): Boolean {
 //        return ContextCompat.checkSelfPermission(
 //            activity,
@@ -63,35 +64,44 @@ class GoogleDriveLauncher(private val activity: Activity) {
         )
     }
 
-    fun onRequestPermissionsResult(
-        requestCode: Int,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            REQUEST_PERMISSION_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 用户授予了相册权限，启动 Google Drive
-                    launchGoogleDrive()
-                    val dialog = AlertDialog.Builder(activity)
-                        .setTitle("应用未安装")
-                        .setMessage("您需要安装 Google Drive 才能使用此功能。")
-                        .setPositiveButton("安装") { dialog, _ ->
-                            // 打开 Google Play 商店的 Google Drive 应用页面
-                            val playStoreIntent = Intent(Intent.ACTION_VIEW)
-                            playStoreIntent.data = Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.docs&hl=zh_TW&gl=US&pli=1")
-                            activity.startActivity(playStoreIntent)
-                            dialog.dismiss()
-                        }
-                        .setNegativeButton("取消") { dialog, _ ->
-                            // 用户取消安装，可以进行其他处理
-                            dialog.dismiss()
-                        }
-                        .create()
-                    dialog.show()
-                }
-            }
-        }
-    }
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        // 将权限请求结果传递给 GoogleDriveLauncher 类处理
+//        googleDriveLauncher.onRequestPermissionsResult(requestCode, grantResults)
+//    }
+//    fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        grantResults: IntArray
+//    ) {
+//        when (requestCode) {
+//            REQUEST_PERMISSION_CODE -> {
+//                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    // 用户授予了相册权限，启动 Google Drive
+//                    launchGoogleDrive()
+//                    val dialog = AlertDialog.Builder(activity)
+//                        .setTitle("应用未安装")
+//                        .setMessage("您需要安装 Google Drive 才能使用此功能。")
+//                        .setPositiveButton("安装") { dialog, _ ->
+//                            // 打开 Google Play 商店的 Google Drive 应用页面
+//                            val playStoreIntent = Intent(Intent.ACTION_VIEW)
+//                            playStoreIntent.data = Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.docs&hl=zh_TW&gl=US&pli=1")
+//                            activity.startActivity(playStoreIntent)
+//                            dialog.dismiss()
+//                        }
+//                        .setNegativeButton("取消") { dialog, _ ->
+//                            // 用户取消安装，可以进行其他处理
+//                            dialog.dismiss()
+//                        }
+//                        .create()
+//                    dialog.show()
+//                }
+//            }
+//        }
+//    }
 
 //    private fun launchGoogleDrive() {
 //        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com"))
@@ -113,47 +123,70 @@ class GoogleDriveLauncher(private val activity: Activity) {
             activity.startActivity(intent)
         } else {
             // 应用未安装，可以提示用户安装
-            // ...
-
+            showGoogleDriveInstallDialog()
         }
     }
 
-    private fun isPackageInstalled(packageName: String): Boolean {
-        val packageManager = activity.packageManager
-        return packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES) != null
-    }
-    private fun showPermissionExplanationDialog() {
-        AlertDialog.Builder(activity)
-            .setTitle("权限说明")
-            .setMessage("为了上传您拍摄的照片到您的 Google Drive 帐户，我们需要访问您的相册。这样我们就可以从您的相册中选择并上传照片。我们将不会访问您的照片以外的任何内容。")
-            .setPositiveButton("确定") { dialog, _ ->
-                // 点击确定按钮后，再次请求相册权限
-                requestPermission()
-                dialog.dismiss()
-            }
-            .setNegativeButton("取消") { dialog, _ ->
-                // 点击取消按钮后，可以进行其他处理，比如关闭应用程序或提供其他功能
-                dialog.dismiss()
-            }
-            .show()
-    }
-    private fun showSignInRequiredDialog() {
-        AlertDialog.Builder(activity)
-            .setTitle("登录 Google 帐户")
-            .setMessage("为了上传您的照片到 Google Drive，您需要先登录您的 Google 帐户。")
-            .setPositiveButton("登录") { dialog, _ ->
-                // 点击登录按钮后，可以启动登录流程，例如使用 Google SignIn API
-                // 也可以跳转到登录界面
-                // 这里仅为示例，你需要根据你的应用程序需求进行实现
+        private fun showGoogleDriveInstallDialog() {
+            AlertDialog.Builder(activity)
+                .setTitle("Google Drive 未安装")
+                .setMessage("您需要安装 Google Drive 才能使用此功能。")
+                .setPositiveButton("安装") { dialog, _ ->
+                    // 打开 Google Play 商店的 Google Drive 应用页面
+                    val playStoreIntent = Intent(Intent.ACTION_VIEW)
+                    playStoreIntent.data =
+                        Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.docs&hl=zh_TW&gl=US&pli=1")
+                    activity.startActivity(playStoreIntent)
+                    dialog.dismiss()
+                }
+                .setNegativeButton("取消") { dialog, _ ->
+                    // 用户取消安装，可以进行其他处理
+                    dialog.dismiss()
+                }
+                .show()
+        }
+
+        private fun isPackageInstalled(packageName: String): Boolean {
+            val packageManager = activity.packageManager
+            return packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES) != null
+        }
+
+        private fun showPermissionExplanationDialog() {
+            AlertDialog.Builder(activity)
+                .setTitle("权限说明")
+                .setMessage("为了上传您拍摄的照片到您的 Google Drive 帐户，我们需要访问您的相册。这样我们就可以从您的相册中选择并上传照片。我们将不会访问您的照片以外的任何内容。")
+                .setPositiveButton("确定") { dialog, _ ->
+                    // 点击确定按钮后，再次请求相册权限
+                    requestPermission()
+                    // 提示用户重新打开应用程序
+                    Toast.makeText(activity, "请重新打开应用程序以使权限生效", Toast.LENGTH_SHORT)
+                        .show()
+                    dialog.dismiss()
+                }
+                .setNegativeButton("取消") { dialog, _ ->
+                    // 点击取消按钮后，可以进行其他处理，比如关闭应用程序或提供其他功能
+                    dialog.dismiss()
+                }
+                .show()
+        }
+
+        private fun showSignInRequiredDialog() {
+            AlertDialog.Builder(activity)
+                .setTitle("登录 Google 帐户")
+                .setMessage("为了上传您的照片到 Google Drive，您需要先登录您的 Google 帐户。")
+                .setPositiveButton("登录") { dialog, _ ->
+                    // 点击登录按钮后，可以启动登录流程，例如使用 Google SignIn API
+                    // 也可以跳转到登录界面
+                    // 这里仅为示例，你需要根据你的应用程序需求进行实现
 //                startSignInProcess()
-                dialog.dismiss()
-            }
-            .setNegativeButton("取消") { dialog, _ ->
-                // 点击取消按钮后，可以进行其他处理，比如关闭应用程序或提供其他功能
-                dialog.dismiss()
-            }
-            .show()
-    }
+                    dialog.dismiss()
+                }
+                .setNegativeButton("取消") { dialog, _ ->
+                    // 点击取消按钮后，可以进行其他处理，比如关闭应用程序或提供其他功能
+                    dialog.dismiss()
+                }
+                .show()
+        }
 //    private fun startSignInProcess() {
 //        // 创建 Google SignIn Intent
 //        val signInIntent = googleSignInClient.signInIntent
